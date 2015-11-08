58eef6aaebcb50e7fa6ea5159d61a69e8c2b1e65
Jenny Mangaly <jmangaly@gmail.com>
2015-11-08 03:46:19 +0000

    Building a Model Test

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,19 +3,17 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM()").first
 	end
 
-  
-
-
 	def create
 		@quote = Quote.create(quote_params)
   	if @quote.invalid?
 			flash[:error] = '<strong>Could not save;</strong> the data you entered is invalid.'
-    	end
+     end
     	redirect_to root_path
   	end
   
   def about
   end
+
 
   private
 
@@ -23,4 +21,6 @@ class QuotesController < ApplicationController
     params.require(:quote).permit(:saying, :author)
   end
 end
+
+
 
